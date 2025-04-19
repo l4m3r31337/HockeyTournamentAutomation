0a80f838969678650f8992ae518f3bf8ebb0bb7d
Александр Павленко <un7uckj@gmail.com>
2025-04-19 18:35:08 +0500

    добавил главную и профиль

--- AutoHockey/Hockey/views.py
+++ AutoHockey/Hockey/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
-from .forms import RegisterForm
+from .forms import RegisterForm, ProfileForm
+from django.views.generic import TemplateView
+
+class IndexView(TemplateView):
+    template_name = 'Hockey/index.html'
+
 
 def register(request):
     if request.method == 'POST':
@@ -12,3 +17,15 @@ def register(request):
     else:
         form = RegisterForm()
     return render(request, 'Hockey/registration.html', {'form': form})
+
+def profile_view(request):
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            # Тут можно сохранить в БД или куда требуется
+            print("Форма сохранена:", form.cleaned_data)
+            return redirect('profile')  # Имя маршрута
+    else:
+        form = ProfileForm()
+
+    return render(request, 'Hockey/profile.html', {'form': form})
